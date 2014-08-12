5b6dc451a69a56d586eb1d113d084e673fee67fd
Eric Roberts <ericroberts@gmail.com>
2014-08-12 15:37:28 -0400

    Get that 100% test coverage. (I think).

--- spec/lib/mutator/helpers_spec.rb
+++ spec/lib/mutator/helpers_spec.rb
@@ -23,27 +23,39 @@ class Stateholder
 end
 
 describe Mutator::Helpers do
-  subject { Stateholder.new }
+  subject { Mutator::Helpers }
 
-  describe '#machine' do
-    it 'should respond to it' do
-      expect(subject).to respond_to :machine
-    end
+  describe '.included' do
+    let(:base) { Stateholder }
 
-    it 'should return a Mutator::Stateholder object' do
-      expect(subject.machine).to be_a Mutator::Stateholder
+    it 'should not error' do
+      expect { subject.included(base) }.to_not raise_error
     end
   end
 
-  [:initial_state, :second_state, :third_state].each do |state|
-    describe ".#{state}" do
-      it 'should respond to the method' do
-        expect(subject.class).to respond_to state
+  context 'when included' do
+    subject { Stateholder.new }
+
+    describe '#machine' do
+      it 'should respond to it' do
+        expect(subject).to respond_to :machine
       end
 
-      it 'should call where on the stateholder with the state as an argument' do
-        expect(subject.class).to receive(:where).with(state: state).and_return(Stateholder.new)
-        subject.class.public_send(state)
+      it 'should return a Mutator::Stateholder object' do
+        expect(subject.machine).to be_a Mutator::Stateholder
+      end
+    end
+
+    [:initial_state, :second_state, :third_state].each do |state|
+      describe ".#{state}" do
+        it 'should respond to the method' do
+          expect(subject.class).to respond_to state
+        end
+
+        it 'should call where on the stateholder with the state as an argument' do
+          expect(subject.class).to receive(:where).with(state: state).and_return(Stateholder.new)
+          subject.class.public_send(state)
+        end
       end
     end
   end
